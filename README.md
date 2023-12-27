8f6b81e706ddf52d21504b207557c90f4747a632
Qwox <qwox@qwox.com>
2023-12-27 21:53:48 +0100

    allow `Span` or `Range<Span>` as macro input

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # Simple `proc_macro` error
 
-[![Github](https://img.shields.io/badge/github-qwox0/simple__proc__macro__error-blue?style=flat&logo=github)](https://github.com/Qwox0/simple_proc_macro_error)
+[![Github](https://img.shields.io/badge/github-Qwox0/simple__proc__macro__error-blue?style=flat&logo=github)](https://github.com/Qwox0/simple_proc_macro_error)
 
 Create simple error messages in Procedural Macros.
+
+# Example
+
+```rust,ignore
+#[proc_macro]
+pub fn my_macro(ts: TokenStream) -> TokenStream {
+    let Some(tt) = ts.into_iter().next() else {
+        return error!("Macro input is empty")
+    };
+    error!(tt.span() => "This macro is not implemented but here is the first token: {tt}")
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,38 @@
 #![forbid(missing_docs)]
 #![forbid(unsafe_code)]
 
-#[cfg(__never_compiled)]
-use crate as _doc;
-
 extern crate proc_macro;
 
 use proc_macro::{Delimiter, Group, Ident, Literal, Punct, Spacing, Span, TokenStream, TokenTree};
-use std::fmt::Arguments;
+use std::{fmt::Arguments, ops::Range};
+
+/// Wrapper around [`Range<Span>`].
+pub struct SpanRange(pub Range<Span>);
+
+impl From<Range<Span>> for SpanRange {
+    fn from(range: Range<Span>) -> Self {
+        Self(range)
+    }
+}
+
+impl From<Span> for SpanRange {
+    fn from(span: Span) -> Self {
+        Self(span..span)
+    }
+}
 
 /// Creates a new error [`TokenStream`] from arguments (created by
-/// [`format_args`]) and start/end [`Span`]s.
-pub fn error_ts(msg: Arguments<'_>, start: Span, end: Span) -> TokenStream {
+/// [`format_args`]) and a [`Range`] of [`Span`]s.
+pub fn error_ts(msg: Arguments<'_>, span: SpanRange) -> TokenStream {
     let msg = match msg.as_str() {
         Some(str) => Literal::string(str),
         None => Literal::string(&msg.to_string()),
     };
     let msg = TokenStream::from(respan(msg, Span::call_site()));
     TokenStream::from_iter([
-        respan(Ident::new("compile_error", start), start),
+        respan(Ident::new("compile_error", span.0.start), span.0.start),
         respan(Punct::new('!', Spacing::Alone), Span::call_site()),
-        respan(Group::new(Delimiter::Brace, msg), end),
+        respan(Group::new(Delimiter::Brace, msg), span.0.end),
     ])
 }
 
@@ -32,18 +44,35 @@ fn respan<T: Into<TokenTree>>(t: T, span: Span) -> TokenTree {
     t
 }
 
-/// Creates a new error [`TokenStream`]. One, two or no [`Span`]s for the
-/// message are possible.
+/// Creates a new proc macro compile time error as a
+/// [`proc_macro::TokenStream`].
+///
+/// The [`std::fmt`] syntax is used for the massage.
+///
+/// Optionally a [`Span`] or a [`Range<Span>`] can be provided (Format:
+/// `error!(span => "Message")`)
+///
+/// # Example
+///
+/// ```rust,ignore
+/// # extern crate proc_macro;
+/// # use proc_macro::TokenStream;
+/// # use simple_proc_macro_error::error;
+/// #[proc_macro]
+/// pub fn my_macro(ts: TokenStream) -> TokenStream {
+///     let Some(tt) = ts.into_iter().next() else {
+///         return error!("Macro input is empty")
+///     };
+///     error!(tt.span() => "This macro is not implemented but here is the first token: {tt}")
+/// }
+/// ```
 #[macro_export]
 macro_rules! error {
     ($span:expr => $($arg:tt)*) => {{
-        $crate::error_ts(format_args!($($arg)*), $span, $span)
-    }};
-    ($start:expr ; $end:expr => $($arg:tt)*) => {{
-        $crate::error_ts(format_args!($($arg)*), $start, $end)
+        $crate::error_ts(format_args!($($arg)*), $crate::SpanRange::from($span))
     }};
     ($($arg:tt)*) => {{
-        $crate::error_ts(format_args!($($arg)*), proc_macro::Span::call_site(), proc_macro::Span::call_site())
+        $crate::error_ts(format_args!($($arg)*), $crate::SpanRange::from(proc_macro::Span::call_site()))
     }};
 }
 
